bd901abcf5548b1507c167796b1865f7b7de0937
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-18 00:26:57 +0300

    New Note Form, use Local storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/*wrapping our app inside router which gives us access to react routing*/}
+    <App/>
+    </BrowserRouter>
   </React.StrictMode>
 )

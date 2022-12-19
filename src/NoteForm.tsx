0ca9e3c8b44e3055db83774baa9243037c286aff
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-19 13:21:13 +0300

    Note list initial commit

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable"
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid";
@@ -16,6 +16,8 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const navigate = useNavigate() //redirect the user
+    
     
     function handleSubmit(e: FormEvent){
         e.preventDefault()
@@ -25,6 +27,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps){
             markdown: markdownRef.current!.value,
             tags: selectedTags,
         })
+        navigate("..")
     }
 
 

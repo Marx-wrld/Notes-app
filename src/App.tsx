f0df146b7db6a03ab8b71b07af4e33388f9b67ec
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-20 15:37:38 +0300

    markdown modules, editnote

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { useMemo } from "react"
 import { v4 as uuidV4 } from "uuid"
 import { NoteList } from "./NoteList"
 import { NoteLayout } from "./NoteLayout"
+import { Note } from "./Note"
+import { EditNote } from "./EditNote"
 
 export type Note = {
   id: string
@@ -50,6 +52,18 @@ function App() {
     })
   }
 
+  function onUpdateNote(id: string,{tags, ...data}: NoteData) {
+    setNotes(prevNotes => {
+    return prevNotes.map(note => {
+      if(note.id === id){
+        return {...note, ...data, tagIds: tags.map(tag => tag.id)}
+      }else{
+        return note
+      }
+    })
+    })
+  }
+
   function addTag(tag: Tag){
     setTags(prev => [...prev, tag])
   }
@@ -60,10 +74,10 @@ function App() {
     <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>}></Route> {/*rendering out a note list*/}
     <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag = {addTag} availableTags={tags}/>}></Route>
     <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
-      <Route index element={<h1>Show</h1>}></Route>
-      <Route path="edit" element={<h1>Edit</h1>}></Route>
+      <Route index element={<Note />}/>
+      <Route path="edit" element={<EditNote onSubmit={onUpdateNote} onAddTag = {addTag} availableTags={tags}/>}/>
     </Route>
-    <Route path="*" element={<Navigate to="/"/>}></Route>
+    <Route path="*" element={<Navigate to="/" />}/>
   </Routes>
   </Container>
   )
